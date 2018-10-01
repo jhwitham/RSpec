50e6668feedfab8e68aa8fe71b550aa7d1399df0
John Witham <john.h.witham@gmail.com>
2018-10-01 16:14:28 +0000

    Add create_posts_spec.rb and requirement for tests

---
+++ spec/features/create_posts_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper.rb'
+
+# visit root route
+# click create post link
+# fill in the form with needed information (title and caption of job)
+# click submit button
+# expect page to have the content we submitted
