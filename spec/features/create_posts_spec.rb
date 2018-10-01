e87a1b45c97fa8e30f42f2f6251a76cb22b58895
John Witham <john.h.witham@gmail.com>
2018-10-01 21:23:29 +0000

    Change test spec from button Create Update to Create Post.

--- spec/features/create_posts_spec.rb
+++ spec/features/create_posts_spec.rb
@@ -16,7 +16,7 @@ feature 'Creating Posts' do
     fill_in 'Title', with: 'title'
     fill_in 'Caption', with: 'caption'
     # click submit button
-    click_button 'Create Update'
+    click_button 'Create Post'
     # expect page to have the content we submitted
     expect(page).to have_content('title')
     expect(page).to have_content('caption')
