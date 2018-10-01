24ac474faa8d0b2c54e01ecd52d7aed99857df4c
John Witham <john.h.witham@gmail.com>
2018-10-01 20:36:52 +0000

    Add tests for filling title & caption, submit button and page having content when submitted

--- spec/features/create_posts_spec.rb
+++ spec/features/create_posts_spec.rb
@@ -7,8 +7,18 @@ require 'rails_helper.rb'
 # expect page to have the content we submitted
 
 feature 'Creating Posts' do
-  # visit root route
-  visit '/'
-  # click create post link
-  click_link 'Create Post'
+  scenario 'can create job' do 
+    # visit root route
+    visit '/'
+    # click create post link
+    click_link 'Create Post'
+    # fill in the form with needed information (title and caption of job)
+    fill_in 'Title', with: 'title'
+    fill_in 'Caption', with: 'caption'
+    # click submit button
+    click_button 'Create Update'
+    # expect page to have the content we submitted
+    expect(page).to have_content('title')
+    expect(page).to have_content('caption')
+  end  
 end
